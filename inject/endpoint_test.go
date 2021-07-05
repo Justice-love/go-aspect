3ae9e2b9ad9a633e20d004549c2a083e84a046ee
Justice-love <eddyxu1213@126.com>
2021-07-05 19:24:58 +0800

    单元测试相关

--- inject/endpoint_test.go
+++ inject/endpoint_test.go
@@ -1,11 +1,23 @@
 package inject
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
 func TestEndpoints(t *testing.T) {
 	arr := endpoints("../testdata/aspect_around.point")
 	fmt.Println(EndpointPrettyText(arr))
+}
+
+func TestOnePoint(t *testing.T) {
+	a := assert.New(t)
+	point := "\tsample.AfterPrint({{c}})\n}"
+	reader := bufio.NewReader(strings.NewReader(point))
+	p := onePoint("point after(main.Do(c Context)) {", reader)
+	a.NotNil(p)
+	a.Equal(&AfterInjectFile{}, p.mode)
 }
--- parse/parse_test.go
+++ parse/parse_test.go
@@ -1,7 +1,10 @@
 package parse
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -29,4 +32,22 @@ func TestInlineFunction(t *testing.T) {
 	line := 10
 	fu := funcInline(f, &line)
 	fmt.Println(fu)
+}
+
+func TestImportParse(t *testing.T) {
+	t.Run("import multiple line", func(t *testing.T) {
+		a := assert.New(t)
+		i := "\t\"bufio\"\n\t\"bytes\"\n\t\"fmt\"\n\t\"github.com/Justice-love/go-aspect/util\"\n\tlog \"github.com/sirupsen/logrus\"\n\t\"io\"\n\t\"os\"\n\t\"strings\"\n)\n"
+		reader := bufio.NewReader(strings.NewReader(i))
+		is, _ := importParse(reader, "import (")
+		a.Equal(8, len(is))
+		a.Equal("log", is[4].ImportTag)
+	})
+	t.Run("import inline", func(t *testing.T) {
+		a := assert.New(t)
+		i := "import \"bytes\""
+		reader := bufio.NewReader(strings.NewReader(i))
+		is, _ := importParse(reader, "import \"bytes\"")
+		a.Equal(1, len(is))
+	})
 }
